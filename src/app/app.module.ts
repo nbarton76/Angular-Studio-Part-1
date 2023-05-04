93ae094dcae1fe09e2c5f5490d445baa12f25b43
nbarton76 <52866856+nbarton76@users.noreply.github.com>
2023-05-03 20:11:10 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-    HeaderComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent
   ],
   imports: [
     BrowserModule
